7c967d4762070e32fd4f1f26aadf4dcc00682347
gyr0byte <gqurav69@gmail.com>
2026-07-22 19:03:28 +0545

    Added a field for resting_ecg, max_HR, and exercise_angina

--- Supervised_Learning/app.py
+++ Supervised_Learning/app.py
@@ -16,3 +16,8 @@ chest_pain = st.selectbox("Chest Pain Type", ['ATA', 'NAP', 'TA', 'ASY'])
 resting_bp = st.number_input('Resting Blood Pressure (mm hg)', 80, 200, 120) 
 cholestrol = st.number_input("Cholestrol (mg/dL)", 100, 600, 200)
 fasting_bs = st.selectbox("Fasting Blood Sugar > 120 mg/dL", [0,1])
+resting_ecg = st.selectbox("Resting ECG",['Normal', 'ST', 'LVH'])
+max_hr = st.slider("Max Heart Rate", 60, 220, 150)
+exercise_angina = st.selectbox("Exercise-Induced Angina", ['Y','N'])
+
+
